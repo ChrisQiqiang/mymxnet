fd09d06c990416bdfe7d12f215cba107cc383f03
unknown <ChrisQiqiang>
2020-07-03 15:42:06 +0800

    fit rebuild

--- example/image-classification/common/fit.py
+++ example/image-classification/common/fit.py
@@ -21,9 +21,21 @@ import os
 import time
 import re
 import math
+import warnings
+import numpy as np
+
 import mxnet as mx
 from mxnet import kvstore
 from mxnet import module
+from mxnet import metric
+from mxnet import ndarray
+from mxnet.context import cpu
+from mxnet.model import BatchEndParam
+from mxnet.initializer import Uniform
+from mxnet.io import DataDesc, DataIter, DataBatch
+from mxnet.base import _as_list
+
+
 
 
 def _chris_update_params_on_kvstore(param_arrays, grad_arrays, kvstore, param_names):
@@ -66,7 +78,104 @@ class MyModule(mx.mod.Module):
                            num_device=len(self._context),
                            kvstore=self._kvstore,
                            param_names=self._exec_group.param_names)
+    def fit(self, train_data, eval_data=None, eval_metric='acc',
+                epoch_end_callback=None, batch_end_callback=None, kvstore='local',
+                optimizer='sgd', optimizer_params=(('learning_rate', 0.01),),
+                eval_end_callback=None,
+                eval_batch_end_callback=None, initializer=Uniform(0.01),
+                arg_params=None, aux_params=None, allow_missing=False,
+                force_rebind=False, force_init=False, begin_epoch=0, num_epoch=None,
+                validation_metric=None, monitor=None, sparse_row_id_fn=None):
+            assert num_epoch is not None, 'please specify number of epochs'
 
+            self.bind(data_shapes=train_data.provide_data, label_shapes=train_data.provide_label,
+                    for_training=True, force_rebind=force_rebind)
+            if monitor is not None:
+                self.install_monitor(monitor)
+            self.init_params(initializer=initializer, arg_params=arg_params, aux_params=aux_params,
+                            allow_missing=allow_missing, force_init=force_init)
+            self.init_optimizer(kvstore=kvstore, optimizer=optimizer,
+                                optimizer_params=optimizer_params)
+
+            if validation_metric is None:
+                validation_metric = eval_metric
+            if not isinstance(eval_metric, metric.EvalMetric):
+                eval_metric = metric.create(eval_metric)
+
+            ################################################################################
+            # training loop
+            ################################################################################
+            for epoch in range(begin_epoch, num_epoch):
+                tic = time.time()
+                eval_metric.reset()
+                nbatch = 0
+                data_iter = iter(train_data)
+                end_of_batch = False
+                next_data_batch = next(data_iter)
+                while not end_of_batch:
+                    data_batch = next_data_batch
+                    if monitor is not None:
+                        monitor.tic()
+                    print("before forward and backward, ",time.time())
+                    self.forward_backward(data_batch)
+                    print("before update, ",time.time())
+                    self.update() #异步执行的
+                    print("before update_metric, ",time.time())
+                    if isinstance(data_batch, list):
+                        self.update_metric(eval_metric,
+                                        [db.label for db in data_batch],
+                                        pre_sliced=True)
+                    else:
+                        self.update_metric(eval_metric, data_batch.label)
+
+                    try:
+                        # pre fetch next batch
+                        next_data_batch = next(data_iter)
+                        self.prepare(next_data_batch, sparse_row_id_fn=sparse_row_id_fn)
+                    except StopIteration:
+                        end_of_batch = True
+
+                    if monitor is not None:
+                        monitor.toc_print()
+
+                    if end_of_batch:
+                        eval_name_vals = eval_metric.get_global_name_value()
+                    print("before batch_end_callback, ",time.time())
+                    if batch_end_callback is not None:
+                        batch_end_params = BatchEndParam(epoch=epoch, nbatch=nbatch,
+                                                        eval_metric=eval_metric,
+                                                        locals=locals())
+                        for callback in _as_list(batch_end_callback):
+                            callback(batch_end_params)
+                    nbatch += 1
+                    print("end of this loop, ",time.time())
+
+                # one epoch of training is finished
+                for name, val in eval_name_vals:
+                    self.logger.info('Epoch[%d] Train-%s=%f', epoch, name, val)
+                toc = time.time()
+                self.logger.info('Epoch[%d] Time cost=%.3f', epoch, (toc-tic))
+
+                # sync aux params across devices
+                arg_params, aux_params = self.get_params()
+                self.set_params(arg_params, aux_params)
+
+                if epoch_end_callback is not None:
+                    for callback in _as_list(epoch_end_callback):
+                        callback(epoch, self.symbol, arg_params, aux_params)
+
+                #----------------------------------------
+                # evaluation on validation set
+                if eval_data:
+                    res = self.score(eval_data, validation_metric,
+                                    score_end_callback=eval_end_callback,
+                                    batch_end_callback=eval_batch_end_callback, epoch=epoch)
+                    #TODO: pull this into default
+                    for name, val in res:
+                        self.logger.info('Epoch[%d] Validation-%s=%f', epoch, name, val)
+
+                # end of 1 epoch, reset the data-iter for another epoch
+                train_data.reset()
 
 def get_epoch_size(args, kv):
     return math.ceil(int(args.num_examples / kv.num_workers) / args.batch_size)
